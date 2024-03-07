80feaa43e0924014a055a3b00eccca140154b9e1
chakrireddy1609 <chakrireddy1609@gmail.com>
2024-03-07 15:54:59 +0530

    new

---
+++ moveZeroes.py
@@ -0,0 +1,10 @@
+def moveZeroes(nums):
+    l = 0
+    for r in range(len(nums)):
+        if nums[r]:
+            nums[l],nums[r]=nums[r],nums[l]
+            l+=1
+    return nums
+
+
+print(moveZeroes([0,1,0,3,12]))
